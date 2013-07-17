5e8f473f39b7ee55fc19dfc22820b9759fb57561
rylon <rylon@web.de>
2013-07-17 23:38:59 +0200

    more configoptions for sai, title, copyright

--- sai/page/default_page/default_page.php
+++ sai/page/default_page/default_page.php
@@ -54,6 +54,8 @@ class default_page extends \SYSTEM\PAGE\Page {
         $vars['menu_sys'] = $this->menu_sys();
         $vars['menu_proj'] = $this->menu_proj();
         $vars['navimg'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_NAVIMG);
+        $vars['title'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_TITLE); //da_sense | Developer Center
+        $vars['copyright'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_COPYRIGHT); //&copy; <a href="http://www.da-sense.de" target="_blank">da_sense</a>, TU Darmstadt 2013
         
         return \SYSTEM\PAGE\replace::replaceFile(\SYSTEM\SERVERPATH(new \SYSTEM\PSAI(),'page/default_page/sai.tpl'), $vars);        
     }
