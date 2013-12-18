7e3f8b576924ca13d9e36ea55d9dc7f182e1f3de
messerbill <ole.hoppe@gmx.de>
2013-12-18 14:58:24 +0100

    added html tags to upload images into galleries

--- sai/modules/saimod_sys_img/saimod_sys_img.php
+++ sai/modules/saimod_sys_img/saimod_sys_img.php
@@ -3,15 +3,22 @@ namespace SYSTEM\SAI;
 
 class saimod_sys_img extends \SYSTEM\SAI\SaiModule {    
     public static function sai_mod__SYSTEM_SAI_saimod_sys_img(){
-        $result = '';
+        //tt
+        $result = '';  
         $img_folders = \SYSTEM\IMG\img::get();
+        $result .= '<form action="upload.php" method="post" enctype="multipart/form-data">'; 
         foreach($img_folders as $name=>$folder){
             $cat = \SYSTEM\IMG\img::get($name);
+            $result .= "<h3>".$name."</h3>";
+            
+            $result .= '<input type="file" name="datei"><br>
+                        <input type="submit" class="btn" value="Hochladen">'; 
             foreach($cat as $img){
-                $result .= '<img src="api.php?call=img&cat='.$name.'&id='.$img.'" alt="" />';   
+                //$result .= '<img src="api.php?call=img&cat='.$name.'&id='.$img.'" alt="" />';   
+                $result .= '<li><a href="api.php?call=img&cat='.$name.'&id='.$img.'">'.$img.'</a></li>';   
             }
         }
-        
+        $result .= "</form>";
         return $result;
     }       
     
