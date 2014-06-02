3a716da9b899ca4887bb8d4e326a4735791180f3
rylon <rylon@web.de>
2014-06-02 02:52:46 +0200

    removed huHU from system inserts, sai lang switcher fix

--- dbd/sql/mysql/data/basic_locale_string.sql
+++ dbd/sql/mysql/data/basic_locale_string.sql
@@ -1,22 +1,22 @@
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_logout', 1, 'Logout', 'Ausloggen', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_login', 1, 'Login', 'Einloggen', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_register', 1, 'Register', 'Registrieren', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_username', 1, 'Username', 'Username', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_password', 1, 'Password', 'Passwort', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_email', 1, 'EMail', 'E-Mail2', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_last_active', 1, 'Last active', 'Zuletzt aktiv', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_join_date', 1, 'Joindate', 'Beitrittsdatum', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_locale', 1, 'Locale', 'Sprache', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_admin_rights', 1, 'Admin Rights', 'Admin Rechte', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_cancel', 1, 'Cancel', 'Abbrechen', '');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_logout', 1, 'Logout', 'Ausloggen');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_login', 1, 'Login', 'Einloggen');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_register', 1, 'Register', 'Registrieren');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_username', 1, 'Username', 'Username');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_password', 1, 'Password', 'Passwort');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_email', 1, 'EMail', 'E-Mail2');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_last_active', 1, 'Last active', 'Zuletzt aktiv');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_join_date', 1, 'Joindate', 'Beitrittsdatum');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_locale', 1, 'Locale', 'Sprache');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_admin_rights', 1, 'Admin Rights', 'Admin Rechte');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_cancel', 1, 'Cancel', 'Abbrechen');
 
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_text_logout', 1, 'Logout before you leave!', 'Loggen Sie sie sich aus bevor Sie gehen!', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_text_login', 1, 'Login to your Website.', 'Loggen Sie sich in ihre Website ein.', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_text_register', 1, 'Register an Account', 'Register an Account', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_text_password_miss', 1, 'Can\'t really remember your Password?', 'Can\'t really remember your Password?', '');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_text_logout', 1, 'Logout before you leave!', 'Loggen Sie sie sich aus bevor Sie gehen!');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_text_login', 1, 'Login to your Website.', 'Loggen Sie sich in ihre Website ein.');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_text_register', 1, 'Register an Account', 'Register an Account');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_text_password_miss', 1, 'Can\'t really remember your Password?', 'Can\'t really remember your Password?');
 
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_placeholder_username', 1, 'peter / peter@world.org', 'peter / peter@world.org', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_placeholder_password', 1, 'my secret123', 'geheim567', '');
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_placeholder_email', 1, 'peter@world.org', 'peter@world.org', '');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_placeholder_username', 1, 'peter / peter@world.org', 'peter / peter@world.org');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_placeholder_password', 1, 'my secret123', 'geheim567');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_placeholder_email', 1, 'peter@world.org', 'peter@world.org');
 
-INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`, `huHU`) VALUES ('basic_state_login', 1, 'You are logged in.', 'You are logged in.', '');
+INSERT INTO `system_locale_string` (`id`, `category`, `enUS`, `deDE`) VALUES ('basic_state_login', 1, 'You are logged in.', 'You are logged in.');
--- sai/page/default_page.php
+++ sai/page/default_page.php
@@ -4,7 +4,7 @@ namespace SYSTEM\SAI;
 
 class default_page extends \SYSTEM\PAGE\Page {
     
-    private function menu_sys(){                
+    private static function menu_sys(){                
         $result = '';
         
         $mods = \SYSTEM\SAI\sai::getSysModules();
@@ -16,7 +16,7 @@ class default_page extends \SYSTEM\PAGE\Page {
         return $result;        
     }
     
-    private function menu_proj(){                
+    private static function menu_proj(){
         $result = '';        
         $mods = \SYSTEM\SAI\sai::getModules();
         foreach($mods as $mod){
@@ -27,21 +27,21 @@ class default_page extends \SYSTEM\PAGE\Page {
         return $result;        
     }
     
-    private function menu_start(){        
+    private static function menu_start(){
         $mod = \SYSTEM\SAI\sai::getStartModule();        
         if(\call_user_func(array($mod, 'right_public')) ||
             \call_user_func(array($mod, 'right_right'))){
             return \call_user_func(array($mod, 'html_li_menu'));}        
         throw new \SYSTEM\LOG\ERROR('Your SAI-Start-Module haz a Problem - either it does not exist or it is not public - which is required!');}
 
-    private function css(){        
+    private static function css(){
         $result = '<link rel="stylesheet" href="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/css/libs/bootstrap.min.css').'" type="text/css" />'.
                   '<link rel="stylesheet" href="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/css/index.css').'" type="text/css" />'.
                   '<link rel="stylesheet" href="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/css/sai_table.css').'" type="text/css" />';        
         return $result;
     }
 
-    private function js(){
+    private static function js(){
         $result = '<script src="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/js/libs/jquery.min.js').'" type="text/javascript"></script>'.
                   '<script src="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/js/libs/bootstrap.min.js').'" type="text/javascript"></script>'.
                   '<script src="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/js/sai.js').'" type="text/javascript"></script>'.
@@ -53,6 +53,19 @@ class default_page extends \SYSTEM\PAGE\Page {
                   '<script src="'.\SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'modules/saimod_sys_locale/tinymce/tinymce.min.js').'" type="text/javascript"></script>';
         return $result;
     }
+    
+    private static function lang_switcher(){
+        $result = '';
+        $langs = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_CONFIG_LANGS);
+        if(in_array('deDE', $langs)){
+            $result .= '<a href="javascript:switchLocale(\'deDE\');"><img src="${PATH_LOCAL_IMG}flag_germany.png" alt="Deutsch"></a>&nbsp;';
+        }
+        
+        if(in_array('enUS', $langs)){
+            $result .= '<a href="javascript:switchLocale(\'enUS\');"><img src="${PATH_LOCAL_IMG}flag_usa.png" alt="English"></a>';
+        }
+        return $result;
+    }
 
     public function html(){
 
@@ -60,12 +73,13 @@ class default_page extends \SYSTEM\PAGE\Page {
         $vars['css'] = $this->css();
         $vars['js'] = $this->js();
 
-        $vars['menu_start'] = $this->menu_start();
-        $vars['menu_sys'] = $this->menu_sys();
-        $vars['menu_proj'] = $this->menu_proj();
+        $vars['menu_start'] = self::menu_start();
+        $vars['menu_sys'] = self::menu_sys();
+        $vars['menu_proj'] = self::menu_proj();
         $vars['navimg'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_NAVIMG);
-        $vars['title'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_TITLE); //da_sense | Developer Center
-        $vars['copyright'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_COPYRIGHT); //&copy; <a href="http://www.da-sense.de" target="_blank">da_sense</a>, TU Darmstadt 2013
+        $vars['title'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_TITLE);
+        $vars['copyright'] = \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_COPYRIGHT);
+        $vars['lang_switcher'] = self::lang_switcher();
         $vars['PATH_LOCAL_IMG'] = \SYSTEM\WEBPATH(new \SYSTEM\PSAI(),'page/img/');
         
         $vars = array_merge($vars,\SYSTEM\locale::getStrings(\SYSTEM\DBD\system_locale_string::VALUE_CATEGORY_SYSTEM_SAI));
