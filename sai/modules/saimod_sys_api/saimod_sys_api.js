b0c0540456b2307cf60cf03cba5d95db706af130
messerbill <ole.hoppe@gmx.de>
2014-06-05 01:33:53 +0200

    api fix

--- sai/modules/saimod_sys_api/saimod_sys_api.js
+++ sai/modules/saimod_sys_api/saimod_sys_api.js
@@ -1,6 +1,7 @@
 var currentID = '';
  
 function init__SYSTEM_SAI_saimod_sys_api() {
+    saimod_sys_api_tree();
     saimod_sys_api_register_clickevents();
     
 }
@@ -79,4 +80,19 @@ function saimod_sys_api_register_clickevents(){
         saimod_sys_api_loadcontent();
     });    
     $('#localetab a').click(function (e) {e.preventDefault(); $(this).tab('show');});
+}
+
+function saimod_sys_api_tree(){
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+        }
+        e.stopPropagation();
+    });
 }
