dc43d022508d2544b09bf862b1f56bfe08d2b9ef
messerbill <ole.hoppe@gmx.de>
2014-06-04 03:41:24 +0200

    api module update. Adding and deleting api calls is now possible via SAI Backend. further updates coming soon.

--- sai/modules/saimod_sys_api/saimod_sys_api.js
+++ sai/modules/saimod_sys_api/saimod_sys_api.js
@@ -1,3 +1,54 @@
-function init__SYSTEM_SAI_saimod_sys_api() {    
+var currentID = '';
+function init__SYSTEM_SAI_saimod_sys_api() {
+    $('tr.api_entries').click(function() {
+        currentID = $(this).attr("id");
+        $('#modaltitle').html("Call ID: "+currentID+'<hr>');
+        $('#modal_description').append('<div id="api_wrap"><b>Group:</b> '+$(this).attr("group")+'<br>');
+        $('#modal_description').append('<b>Type:</b> '+$(this).attr("typ")+'<br>');
+        $('#modal_description').append('<b>ParentID:</b> '+$(this).attr("parentID")+'<br>');
+        $('#modal_description').append('<b>ParentValue:</b> '+$(this).attr("parentValue")+'<br>');
+        $('#modal_description').append('<b>Name:</b> '+$(this).attr("name")+'<br>');
+        $('#modal_description').append('<b>Verify:</b> '+$(this).attr("verify")+'</div>');
+        $('#modal_api').modal('show');
+    });
+    $('#del_api').click(function() {
+        $.ajax({
+            url: SAI_ENDPOINT,
+                            data: { sai_mod: '.SYSTEM.SAI.saimod_sys_api',
+                                    action: 'deletecall',
+                                    ID: currentID},
+                                type: 'GET',
+                                success: function(data) {
+                                    console.log("api call deleted");
+                                    $('#modal_api').modal('hide');
+                                }
+            });
+    });
+    $('#addcall').click(function() {
+        console.log("bla"); 
+            var id = $('#new_call_id').val();
+            var group = $('#new_call_group').val();
+            var type = $('#new_call_type').val();
+            var parentid = $('#new_call_parentid').val();
+            var parentvalue = $('#new_call_parentvalue').val();
+            var name = $('#new_call_name').val();
+            var verify = $('#new_call_verify').val();
+            $.ajax({
+            url: SAI_ENDPOINT,
+                            data: { sai_mod: '.SYSTEM.SAI.saimod_sys_api',
+                                    action: 'addcall',
+                                    ID: id,
+                                    group: group,
+                                    type: type,
+                                    parentID: parentid,
+                                    parentValue : parentvalue,
+                                    name: name,
+                                    verify: verify},
+                                type: 'GET',
+                                success: function(data) {
+                                    console.log("new api call added");
+                                }
+            });});
+        
     $('#localetab a').click(function (e) {e.preventDefault(); $(this).tab('show');});
 }
--- sai/modules/saimod_sys_api/tabs.tpl
+++ sai/modules/saimod_sys_api/tabs.tpl
@@ -8,3 +8,46 @@
         ${tabs}        
     </div>
 </div>
+ <table class="table table-hover table-condensed" style="overflow: auto;">
+        <tr>
+            <th>ID</th>
+            <th>Group</th>
+            <th>Type</th>
+            <th>ParentID</th>
+            <th>ParentValue</th>
+            <th>Name</th>
+            <th>Verify</th>
+        </tr>
+        <tr>
+            <td><input type="text" id="new_call_id" placeholder="new id" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_group" placeholder="new group" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_type" placeholder="new type" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_parentid" placeholder="parent id" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_parentvalue" placeholder="parent value" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_name" placeholder="name" style="width: 140px;"></td>
+            <td><input type="text" id="new_call_verify" placeholder="verify" style="width: 140px;"></td>
+        </tr>
+        
+    </table>
+    <button type="button" class="btn-small btn-success" id="addcall">Add</button>
+    
+<div class="modal fade" id="modal_api" style="width: 700px;" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="modaltitle"></h4>
+        <div id="modal_description"></div>
+      </div>
+        <div class="modal-body" id="modaltextarea" style="display: none;">
+            <input type="text" id="new_call_id" placeholder="new call id" name="content" style="width:100%" />
+      </div>
+      </div>
+      <div class="modal-footer">
+          <button type="button" id="del_api" class="btn-small btn-danger" style="float: left;">Delete</button>
+          <button type="button" class="btn-small" data-dismiss="modal" id="edit_close">Close</button>
+<!--          <button type="button" class="btn-small btn-success" id="new_call">Add</button>-->
+      </div>
+        
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
