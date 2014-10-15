a2981812193ac0f071673b75191b2b99e6c70043
rylon <rylon@web.de>
2014-10-16 00:29:18 +0200

    fixed merge problem, pg adjustments, pg schema

--- sai/modules/saimod_sys_cron/qq/SYS_SAIMOD_CRON.php
+++ sai/modules/saimod_sys_cron/qq/SYS_SAIMOD_CRON.php
@@ -5,7 +5,7 @@ class SYS_SAIMOD_CRON extends \SYSTEM\DB\QQ {
     protected static function query(){
         return new \SYSTEM\DB\QQuery(get_class(),
 //pg            
-'',
+'SELECT * FROM '.\SYSTEM\DBD\system_cron::NAME_PG.' ORDER BY class;',
 //mys
-'SELECT * FROM system_cron ORDER BY class;'
+'SELECT * FROM '.\SYSTEM\DBD\system_cron::NAME_MYS.' ORDER BY class;'
 );}}
