8a3e1650e630df3ea2119e6977a5a303214048a7
rylon <rylon@web.de>
2014-11-03 14:36:59 +0100

    small fix in replace if you put null as vars

--- page/replace.php
+++ page/replace.php
@@ -6,27 +6,19 @@ class replace
 {
 
         public static function replace($text, $vars){
-
+            if(!$vars){
+                $vars = array();}
             $search = array();
             $replace = array();
 
             foreach(array_keys($vars) as $var){
-                $search[] = '/\${'.$var.'}/';
-            }
+                $search[] = '/\${'.$var.'}/';}
 
             foreach($vars as $var){
-                $replace[] = $var;
-            }
-
+                $replace[] = $var;}
             return @preg_replace($search, $replace, $text);
         }
-
         public static function replaceFile($path, $vars){
-
             $buffer = file_get_contents($path);            
-
-            return self::replace($buffer, $vars);
-
-        }
-
+            return self::replace($buffer, $vars);}
 }
