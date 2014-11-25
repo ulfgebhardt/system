c83b5a03f95e08e6c38fea2c80715defdaab3262
rylon <rylon@web.de>
2014-11-25 18:31:20 +0100

    check for class_exists in State

--- page/State.php
+++ page/State.php
@@ -8,9 +8,9 @@ class State {
         $res = \SYSTEM\DBD\SYS_PAGE_GROUP::QQ(array($group));
         while($row = $res->next()){
             $row['css'] = $row['js'] = array();
-            if(\method_exists($row['php_class'], 'css') && \is_callable($row['php_class'].'::css')){
+            if(\class_exists($row['php_class']) && \method_exists($row['php_class'], 'css') && \is_callable($row['php_class'].'::css')){
                 $row['css'] = array_merge($row['css'], call_user_func($row['php_class'].'::css'));}
-            if(\method_exists($row['php_class'], 'js') && \is_callable($row['php_class'].'::js')){
+            if(\class_exists($row['php_class']) && \method_exists($row['php_class'], 'js') && \is_callable($row['php_class'].'::js')){
                 $row['js'] = array_merge($row['js'], call_user_func($row['php_class'].'::js'));}
             $result[] = $row;}
         return \SYSTEM\LOG\JsonResult::toString($result);}
