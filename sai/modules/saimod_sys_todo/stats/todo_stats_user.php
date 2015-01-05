bde461ba8c7fed6ab4786bc0429d8b17bc0efca2
rylon <rylon@web.de>
2015-01-05 16:04:05 +0100

    fixed division by zero statistics bug, system_cron sql data

--- sai/modules/saimod_sys_todo/stats/todo_stats_user.php
+++ sai/modules/saimod_sys_todo/stats/todo_stats_user.php
@@ -6,5 +6,7 @@ class todo_stats_user extends todo_stats {
         $res = array();
         $res[0] = \SYSTEM\DBD\SYS_SAIMOD_TODO_STATS_COUNT_TODO_USER::Q1();
         $res[2] = \SYSTEM\DBD\SYS_SAIMOD_TODO_STATS_COUNT_DOTO_USER::Q1();
-        return new \SYSTEM\SAI\todo_stats_data('User ToDos',floatval($res[2]['count']),floatval($res[0]['count']+$res[2]['count']));}
+        $count = floatval($res[2]['count']);
+        $all = floatval($res[0]['count']+$res[2]['count']);
+        return $all == 0 ? new \SYSTEM\SAI\todo_stats_data('User ToDos',1,1) : new \SYSTEM\SAI\todo_stats_data('User ToDos',$count,$all);}
 }
